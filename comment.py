a8648d6c035cb23d963a52137b2fa8965ca4ffb0
Rudra061 <58043374+Rudra061@users.noreply.github.com>
2024-01-01 20:36:38 +0530

    Sentiment analysis using textblob

--- comment.py
+++ comment.py
@@ -1,3 +1,5 @@
+import csv
+import re
 from googleapiclient.discovery import build
 
 
@@ -6,7 +8,7 @@ class YouTubeCommentsReader:
         self.api_key = api_key
         self.youtube = build("youtube", "v3", developerKey=self.api_key)
 
-    def get_video_comments(self, video_id):
+    def get_video_comments(self, video_id, max_comments=1000):
         request = self.youtube.commentThreads().list(
             part="snippet",
             videoId=video_id,
@@ -18,7 +20,7 @@ class YouTubeCommentsReader:
         comments = []
         next_page_token = None
 
-        while True:
+        while len(comments) < max_comments:
             request = self.youtube.commentThreads().list(
                 part="snippet",
                 videoId=video_id,
@@ -35,7 +37,34 @@ class YouTubeCommentsReader:
 
             next_page_token = response.get("nextPageToken")
 
-            if not next_page_token:
+            if not next_page_token or len(comments) >= max_comments:
                 break  # No more pages
 
-        return comments
+        return comments[:max_comments]
+
+
+class CommentCSVWriter:
+
+    def __init__(self, csv_file_name="comments.csv"):
+        self.csv_file_name = csv_file_name
+
+    def write_to_csv(self, data):
+        with open(self.csv_file_name, mode='w', newline='', encoding='utf-8') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            for comment in data:
+                csv_writer.writerow([comment])
+
+
+class Video_url:
+
+    def __init__(self, url):
+        self.url = url
+
+    def extract_video_id(self):
+        """
+            Extracts the video ID from a YouTube URL using regular expressions.
+        """
+        video_id = re.search(r'(?<=v=)[^&#]+', self.url)
+        video_id = video_id or re.search(r'(?<=be/)[^&#]+', self.url)
+
+        return video_id.group(0) if video_id else None
--- main.py
+++ main.py
@@ -1,31 +1,62 @@
 import os
 from dotenv import load_dotenv, find_dotenv
-import csv
-from comment import YouTubeCommentsReader
+from comment import YouTubeCommentsReader, CommentCSVWriter, Video_url
+from textblob import TextBlob
+import matplotlib.pyplot as plt
+
+
+
+def analyze_sentiment(comment):
+    analysis = TextBlob(comment)
+    polarity = analysis.sentiment.polarity
+    # Classify the polarity of the comment
+    if polarity > 0.1:
+        return 'Positive'
+    elif -0.1 <= polarity <= 0.1:
+        return 'Neutral'
+    else:
+        return 'Negative'
+
 
 if __name__ == "__main__":
     # Reading the API_KEY
     load_dotenv(find_dotenv())
     api_key = os.getenv("YOUTUBE_API_KEY")
-    print(api_key)
-    # Replace 'YOUR_VIDEO_ID' with the actual video ID you want to retrieve comments for
 
-    video_id = "YmwskGLycHo"
+    # Enter the YouTube video link
+    input_url = Video_url(input("Enter Video Url : "))
+
+    video_id = input_url.extract_video_id()
+    print(video_id)
 
     # Create an instance of YouTubeCommentsReader
     youtube_comments_reader = YouTubeCommentsReader(api_key)
 
-    # Get and print the comments for the specified video
+    # Get the comments for the specified video
     video_comments = youtube_comments_reader.get_video_comments(video_id)
+    print(len(video_comments))
 
-    csv_file_name = "comments.csv"
+    # writing the comments into a csv file
+    csv_writer = CommentCSVWriter()
+    csv_writer.write_to_csv(video_comments)
 
-    # Write the data to the CSV file
-    with open(csv_file_name, mode='w', newline='',  encoding='utf-8') as csv_file:
-        csv_writer = csv.writer(csv_file)
+    print("Comments fetched, starting analysis")
 
-        # Write each row of the data list to the CSV file
-        for comment in video_comments:
-            csv_writer.writerow([comment])
+    sentiment_counts = {'Positive': 0, 'Neutral': 0, 'Negative': 0}
 
-    print(f"Data has been written to {csv_file_name}.")
+    # Analyze the sentiment for each comment
+    for comment in video_comments:
+        sentiment = analyze_sentiment(comment)
+        sentiment_counts[sentiment] += 1
+        #print(f"Comment: {comment} \nSentiment: {sentiment}\n")
+
+    # Draw a pie chart
+    labels = sentiment_counts.keys()
+    sizes = sentiment_counts.values()
+
+    fig, ax = plt.subplots()
+    ax.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90, colors=['lightgreen', 'lightblue', 'lightcoral'])
+    ax.axis('equal')  # Equal aspect ratio ensures the pie chart is circular.
+
+    plt.title('Sentiment Distribution')
+    plt.show()
